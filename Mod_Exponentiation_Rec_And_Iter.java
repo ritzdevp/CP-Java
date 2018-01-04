45931f7ecaa82fee556631c51a25fa6e6c59e77e
ritzdevp <rtjsingh30@gmail.com>
2018-01-04 22:19:07 +0530

    added modular exponentiation iterative

--- Mod_Exponentiation_Rec_And_Iter.java
+++ Mod_Exponentiation_Rec_And_Iter.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Mod_Exponentiation_Rec_And_Iter {
 
-	// (a^b)%c
+	// (a^b)%c ; c is the modulus
 	public static int modRecursive(int a, int b, int c){
 		
 		if (b==0){
@@ -19,12 +19,31 @@ public class Mod_Exponentiation_Rec_And_Iter {
 		
 	}
 	
+	public static int modIterative(int a, int b, int c){
+		int result = 1;
+		
+		while (b>0){
+			//if odd
+			if (b%2!=0){
+				result  = ( result * a ) % c ;
+			}
+			
+			a = ( a * a ) % c;
+			
+			b = b / 2;
+		}
+		
+		return result%c;
+	}
+	
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
 		int b = s.nextInt();
 		int c = s.nextInt();
 		System.out.println(modRecursive(a, b, c));
+		System.out.println(modIterative(a, b, c));
+		
 		
 		
 	}
